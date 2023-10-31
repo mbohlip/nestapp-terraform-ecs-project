a5c35588ff4c6e739aa1d14a8e7810d82244e468
Peter Mbohli <mbohlip@yahoo.com>
2023-10-31 01:20:45 -0700

    sg

--- _variable.tf
+++ _variable.tf
@@ -16,57 +16,47 @@ variable "environment" {
 
 # vpc variables
 variable "vpc_cidr" {
-  default       = "10.0.0.0/16"
   description   = "VPC CIDR Block"
   type          = string
 }
 
 variable "public_subnet_AZ1_cidr" {
-  default       = "10.0.0.0/24"
   description   = "Public Subnet AZ1 cidr block"
   type          = string
 }
 
 variable "public_subnet_AZ2_cidr" {
-  default       = "10.0.1.0/24"
   description   = "Public Subnet AZ2 cidr block"
   type          = string
 }
 
 variable "private_app_subnet_AZ1_cidr" {
-  default       = "10.0.2.0/24"
   description   = "Private App Subnet AZ1 cidr block"
   type          = string
 }
 
 variable "private_app_subnet_AZ2_cidr" {
-  default       = "10.0.3.0/24"
   description   = "Private App Subnet AZ2 cidr block"
   type          = string
 }
 
 variable "private_data_subnet_AZ1_cidr" {
-  default       = "10.0.4.0/24"
   description   = "Private Data Subnet AZ1 cidr block"
   type          = string
 }
 
 variable "private_data_subnet_AZ2_cidr" {
-  default       = "10.0.5.0/24"
   description   = "Private Data Subnet AZ2 cidr block"
   type          = string
 }
 
-################################################
-
-
-
 # Security group variables
 variable "ssh_location" {
-  default       = "0.0.0.0/0"
-  description   = "the ip address that can ssh into the ec2"
+  description   = "the ip address that can ssh into the server"
   type          = string
 }
+
+################################################
 
 # RDS variables
 variable "db_snapshots_identifier" {
